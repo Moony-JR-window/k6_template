9c4a7df8b7bdddcf603c75abb612a98f976daac3
mony.rorn <mony.rorn@wingbank.com.kh>
2025-05-21 22:23:29 +0700

    resolve@2.0.0

--- config/cofig.js
+++ config/cofig.js
@@ -1,6 +1,17 @@
 
+import { parseCSV } from '../build/csv-loader.js';
+import { SharedArray } from 'k6/data';
 
 export const config = {
   baseUrl: 'https://test.k6.io',
   payment: open('../data/payment_data.csv'), // Parsed structured data
 };
+
+
+
+export function callCvsfile(fileName) {
+  let paymentData = new SharedArray('payment_data', () => parseCSV(fileName));
+
+  let data = paymentData[__VU % paymentData.length];
+  return data;
+}
--- src/testSimple.js
+++ src/testSimple.js
@@ -1,9 +1,8 @@
 import http from 'k6/http';
 import { check } from 'k6';
 import * as encrypted from '../build/cryptoEs.js';
-import { config} from '../config/cofig.js';
-import { parseCSV } from '../build/csv-loader.js';
-import { SharedArray } from 'k6/data';
+import { config,callCvsfile} from '../config/cofig.js';
+
 
 export default function () {
     const res = http.get(config.baseUrl);
@@ -13,10 +12,7 @@ export default function () {
 }
 
 const text = encrypted.Encrypted("Hello", "Hello");
-
-let paymentData =  new SharedArray('payment_data', () => parseCSV(config.payment));
-
-let user = paymentData[__VU % paymentData.length];
+const user = callCvsfile(config.payment)
 console.log(`Using user LOGIN_ID2: ${user.LOGIN_ID2}`);
 console.log(`Using user LOGIN_ID3: ${user.LOGIN_ID3}`);
 
