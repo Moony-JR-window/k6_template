a4d8f84e24a8a880e561776b564e6cfefca9304f
mony.rorn <mony.rorn@wingbank.com.kh>
2025-05-23 21:15:17 +0700

    fixIssueCSV@1.0.0

--- src/WingPos.js
+++ src/WingPos.js
@@ -4,28 +4,25 @@ import * as encrypted from '../build/cryptoEs.js';
 import { config, callCvsfile } from '../config/cofig.js';
 
 
-// let csv = callCvsfile(config.csvFiles.accounts);
-// console.log("========== this is round", csv.round);
-// let phoneNumber = encrypted.encrypt(csv.acc);
-
-
 export default function AccessToken() {
     let csv = callCvsfile('accounts');
     console.log("========== this is round fun", csv.round);
-    // const data = {
-    //     username: csv.acc,
-    //     password: phoneNumber,
-    //     client_id: 'mobileapps_test',
-    //     client_secret: '202122232425262728292a2b2c2d2e2f',
-    //     device_id: 'd473f4aa-ebca-4106-a659-8deb4a7216f8',
-    //     grant_type: 'password',
-    //     application_id: 'WINGPAY',
-    // };
-    // const headers = {
-    //     'Content-Type': 'application/x-www-form-urlencoded',
-    // };
-
-    // const res = http.post(config.wingUrl + "/oauth/token", data, { headers });
-    // let responseBody = JSON.parse(res.body);
-    // console.log('Response body:', responseBody.access_token);
+    let phoneNumber = encrypted.encrypt(csv.acc)
+    const data = {
+        username: csv.acc,
+        password: phoneNumber,
+        client_id: 'mobileapps_test',
+        client_secret: '202122232425262728292a2b2c2d2e2f',
+        device_id: 'd473f4aa-ebca-4106-a659-8deb4a7216f8',
+        grant_type: 'password',
+        application_id: 'WINGPAY',
+    };
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    console.log(data);
+    
+    const res = http.post(config.wingUrl + "/oauth/token", data, { headers });
+    let responseBody = JSON.parse(res.body);
+    console.log('Response body:', responseBody.access_token);
 }
